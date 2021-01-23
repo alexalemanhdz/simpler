2b327a1ba0db29d081947ef2f9f604d665aac8ab
alexalemanhdz <alex.aleman.public@gmail.com>
2021-01-23 14:13:59 -0600

    Added props to Icon container

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Icon = () => <p>Icon</p>;
+const Container = styled.div``;
+
+const Icon = ({ icon, name, url }) => (
+  <Container>
+    <p>Hola</p>
+  </Container>
+);
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default Icon;
