c755f7cb6fd6b388d551d968d456f0024e6193e8
alexalemanhdz <alex.aleman.public@gmail.com>
2021-01-23 13:27:58 -0600

    Added background color

--- src/components/Desktop/index.js
+++ src/components/Desktop/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import theme from '../../utils/theme';
+
 const Container = styled.div`
-  background-color: red;
+  background-color: ${theme.palette.background.primary};
+  height: 100%;
+  width: 100%;
 `;
 
 const Desktop = () => (
---
+++ src/utils/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  palette: {
+    background: {
+      primary: '#008080',
+    },
+  },
+};
+
+export default theme;
