3808f8d41e2fbd2eb98e7bf051d313708db63d05
alexalemanhdz <alex.aleman.public@gmail.com>
2021-01-23 13:09:33 -0600

    Added babel plugin for Styled Components

--- src/components/Desktop/index.js
+++ src/components/Desktop/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: red;
+`;
 
 const Desktop = () => (
-  <p>Hello Desktop</p>
+  <Container>
+    <p>Hello Desktop</p>
+  </Container>
 );
 
 export default Desktop;
